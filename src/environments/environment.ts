a4d19a34a87fe641028ebe3d2d4d96755906ae66
Spencer Greeff <spencer4908@live.com>
2024-09-07 01:03:24 -0600

    Updated environment for prod.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // providersAPI: 'assets/providers.json'
-  providersAPI: 'https://healthcare.lndo.site/rest/custom/fst_physicians.json'
+  providersAPI: 'assets/providers.json'
+  // providersAPI: 'https://healthcare.lndo.site/rest/custom/fst_physicians.json'
 };
